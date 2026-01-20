818b988e1f32627852b6691e42706f6407975a0e
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2026-01-20 10:22:55 +0530

    initial commit

---
+++ src/main/java/com/sc/ecom/cmgmt/ScEcomCustMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sc.ecom.cmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScEcomCustMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScEcomCustMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc/ecom/cmgmt/ScEcomCustMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.ecom.cmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScEcomCustMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e41b7d8847422307fe136dfdd9ad7afeef5e0690
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2026-01-22 09:55:05 +0530

    Implemented review comments

--- src/main/java/com/sc/ecom/cmgmt/controllers/CustomerManagementController.java
+++ src/main/java/com/sc/ecom/cmgmt/controllers/CustomerManagementController.java
@@ -8,12 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/customers")
 public class CustomerManagementController {
 
+    /**
+     *  Request url path: <a href="http://localhost:8080/api/v1/customers/info">info</a>
+     * */
     @RequestMapping(path = "/info", method = RequestMethod.GET)
     public String getCustomerInfo() {
         System.out.println("getCustomerInfo");
         return "Customer Information";
     }
 
+    /**
+     *  Request url path: <a href="http://localhost:8080/api/v1/customers/orders">orders</a>
+     * */
     @RequestMapping(path = "/orders", method = RequestMethod.GET)
     public String getCustomerOrders() {
         System.out.println("getCustomerOrders");
